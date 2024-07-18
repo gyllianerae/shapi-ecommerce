56bf21d6c79edb27f260f8e3385bb25203a43231
gyllianerae <100182300+gyllianerae@users.noreply.github.com>
2024-07-19 00:26:18 +0800

    updated url for backend

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    fetch('https://shapi-ecommerce-backend.onrender.com/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data));
   },[])
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -18,7 +18,7 @@ const LoginSignup = () => {
   const login = async () =>{
     console.log('login function executed',formData);
     let responseData;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://shapi-ecommerce-backend.onrender.com/login',{
       method:'POST',
       headers:{
         Accept:'application/json',
@@ -39,7 +39,7 @@ const LoginSignup = () => {
   const signup = async () =>{
     console.log('signup function executed',formData);
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://shapi-ecommerce-backend.onrender.com/signup',{
       method:'POST',
       headers:{
         Accept:'application/json',
