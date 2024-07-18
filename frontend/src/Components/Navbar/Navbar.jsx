6ec7960a8bad642fc6fd6301561cc08592ff8fad
gyllianerae <100182300+gyllianerae@users.noreply.github.com>
2024-07-18 23:42:11 +0800

    fixed bugs

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useContext } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
-// import { ShopContext } from '../../Context/ShopContext'
+import { ShopContext } from '../../Context/ShopContext'
 import nav_dropdown from '../Assets/nav_dropdown.png'
 
 
 const Navbar = () => {
     
     const [menu,setMenu] = useState("shop");
-    // const {getTotalCartItems} = useContext(ShopContext);
+    const {getTotalCartItems} = useContext(ShopContext);
     const menuRef = useRef();
 
     const dropdown_toggle = (e) => {
@@ -37,7 +37,7 @@ const Navbar = () => {
             ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
             :<Link to="/login"><button>Login</button></Link>}
             <Link to="/cart"><img src={cart_icon} alt="" /></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
     </div>
   )
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -79,7 +79,17 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart,getTotalCartAmount};
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                totalItem+=cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart,getTotalCartAmount,getTotalCartItems};
 
     return (
         <ShopContext.Provider value={contextValue}>
