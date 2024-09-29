db9c36803e19e11c49d9ff1bf65a5500ce8def99
gyllianerae <100182300+gyllianerae@users.noreply.github.com>
2024-09-29 14:44:53 +0800

    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://shapi-ecommerce-frontend.onrender.com"
 }));
 
 // Database connection with mongodb
